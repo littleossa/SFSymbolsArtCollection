b7f6e9a69d7f483ae91f275499904d8abf11763c
littleossa <littleossa@gmail.com>
2022-04-26 05:12:03 +0900

    Initial Commit

---
+++ SFSymbolsArtCollection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SFSymbolsArtCollection
+//
+//  Created by 平岡修 on 2022/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SFSymbolsArtCollection/SFSymbolsArtCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolsArtCollectionApp.swift
+//  SFSymbolsArtCollection
+//
+//  Created by 平岡修 on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolsArtCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

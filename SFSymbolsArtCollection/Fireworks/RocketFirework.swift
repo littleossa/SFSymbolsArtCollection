9d80487e94d9eb500361b65d3889bfa71fdfcf0f
littleossa <littleossa@gmail.com>
2022-09-03 07:59:00 +0900

    [refactor] RocketFirework

--- SFSymbolsArtCollection/Fireworks/RocketFirework.swift
+++ SFSymbolsArtCollection/Fireworks/RocketFirework.swift
@@ -7,43 +7,48 @@
 
 import SwiftUI
 
+extension RocketFirework {
+    
+    init(baseLength: CGFloat, color: Color) {
+        self.baseLength = baseLength
+        self.color = color
+        self.alternatingCurrentOffsetYs = [
+            CGFloat(baseLength * -0.22),
+            CGFloat(0),
+            CGFloat(baseLength * 0.22),
+        ]
+    }
+}
+
 struct RocketFirework: View {
     
+    let baseLength: CGFloat
     let color: Color
+    let alternatingCurrentOffsetYs: [CGFloat]
     
     var body: some View {
         
         ZStack {
-            DrawingSymbol(.circleFill,
-                          color: color,
-                          width: 20,
-                          height: 20,
-            offsetY: -140)
+            Image(symbol: .circleFill)
+                .arrangeShape(color: color,
+                              fontSize: baseLength * 0.05,
+                              offsetY: baseLength * -0.35)
             
-            DrawingSymbol(.alternatingcurrent,
-                          color: color,
-                          width: 100,
-                          height: 10,
-                          rotationDegrees: 90,
-            offsetY: -89)
-            DrawingSymbol(.alternatingcurrent,
-                          color: color,
-                          width: 100,
-                          height: 10,
-                          rotationDegrees: 90)
-            
-            DrawingSymbol(.alternatingcurrent,
-                          color: color,
-                          width: 100,
-                          height: 10,
-                          rotationDegrees: 90,
-            offsetY: 89)
+            ForEach(alternatingCurrentOffsetYs, id: \.self) { offsetY in
+                Image(symbol: .alternatingcurrent)
+                    .arrangeShape(color: color,
+                                  width: baseLength * 0.25,
+                                  height: baseLength * 0.025,
+                                  rotationDegrees: baseLength * 0.225,
+                                  offsetY: offsetY)
+            }
         }
     }
 }
 
 struct RocketFirework_Previews: PreviewProvider {
     static var previews: some View {
-        RocketFirework(color: .red)
+        RocketFirework(baseLength: 400,
+                       color: .red)
     }
 }

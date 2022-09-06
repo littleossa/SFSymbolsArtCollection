48e73ec00bb0179099a3f9927ee1cf3d10ba9f40
littleossa <littleossa@gmail.com>
2022-09-06 23:24:26 +0900

    [update] ArmHair

--- SFSymbolsArtCollection/ArtWorks/ArmHair/ArmHair.swift
+++ SFSymbolsArtCollection/ArtWorks/ArmHair/ArmHair.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ArmHair: View {
+    
     var body: some View {
         
         GeometryReader { proxy in
             
+            let baseLength = proxy.baseLength
+            
+            let lowestX = Int(baseLength * -0.35)
+            let highestX = Int(baseLength * 0.1)
+            let lowestY = Int(baseLength * -0.075)
+            let highestY = Int(baseLength * 0.02)
+            
             ZStack {
-                Arm(baseLength: 400)
+                Arm(baseLength: baseLength)
                 
                 ForEach(0..<250) { _ in
-                    Hair(baseLength: 400)
-                        .offset(x: CGFloat(Int.random(in: -140...40)),
-                                y: CGFloat(Int.random(in: -30...8)))
+                    Hair(baseLength: baseLength)
+                        .offset(x: CGFloat(Int.random(in: lowestX...highestX)),
+                                y: CGFloat(Int.random(in: lowestY...highestY)))
                 }
             }
             .position(x: proxy.frame(in: .local).midX,

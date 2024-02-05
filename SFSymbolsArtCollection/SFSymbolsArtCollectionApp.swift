bf116f03aee606c0e837714e5c00977f0eb95c18
littleossa <littleossa@gmail.com>
2024-02-06 08:59:39 +0900

    Add preferredColorScheme to light

--- SFSymbolsArtCollection/SFSymbolsArtCollectionApp.swift
+++ SFSymbolsArtCollection/SFSymbolsArtCollectionApp.swift
@@ -14,6 +14,7 @@ struct SFSymbolsArtCollectionApp: App {
         WindowGroup {
             GeometryReader { geometry in
                 ArtGalleryView(screenSize: geometry.size)
+                    .preferredColorScheme(.light)
             }
         }
     }

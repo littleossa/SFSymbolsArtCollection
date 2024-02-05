3871c7ed47b558b982ea4d5b8447fd5d280a5296
littleossa <littleossa@gmail.com>
2024-02-06 07:21:56 +0900

    Update ArtGallery layout

--- SFSymbolsArtCollection/ArtisticWorldOfSFSymbols/ArtGalleryView.swift
+++ SFSymbolsArtCollection/ArtisticWorldOfSFSymbols/ArtGalleryView.swift
@@ -11,23 +11,59 @@ extension ArtGalleryView {
     
     init(screenSize: CGSize) {
         self.model = ArtWorkModel(screenSize: screenSize)
+        self.columns = Array(repeating: GridItem(spacing: model.galleryColumnSpacing),
+                             count: model.galleryColumnCount)
     }
 }
 
 struct ArtGalleryView: View {
     
     private let model: ArtWorkModel
+    private let columns: [GridItem]
     
     var body: some View {
         NavigationStack {
-            List {
-                NavigationLink {
-                    AfroBoyArtView()
-                } label: {
-                    Text("Afro boy")
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    
+                    Group {
+                        NavigationLink {
+                            AfroBoyArtView()
+                        } label: {
+                            AfroBoyPreviewView()
+                        }
+                        
+                        NavigationLink {
+                            AfroBoyArtView()
+                        } label: {
+                            AfroBoyPreviewView()
+                        }
+                        
+                        NavigationLink {
+                            AfroBoyArtView()
+                        } label: {
+                            AfroBoyPreviewView()
+                        }
+                        
+                        NavigationLink {
+                            AfroBoyArtView()
+                        } label: {
+                            AfroBoyPreviewView()
+                        }
+                        
+                        NavigationLink {
+                            AfroBoyArtView()
+                        } label: {
+                            AfroBoyPreviewView()
+                        }
+                    }
+                    .padding(model.galleryColumnSpacing)
+                    .background(RoundedRectangle(cornerRadius: 12).stroke(lineWidth: 4))
+                    .padding(.top, 24)
                 }
+                .padding(.horizontal, 24)
             }
-            .navigationTitle("Gallery")
+            .navigationTitle("Art Gallery")
         }
         .environment(model)
     }

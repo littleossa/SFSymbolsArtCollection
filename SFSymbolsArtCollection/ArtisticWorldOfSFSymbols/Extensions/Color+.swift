bf0d7342d09b8aa0fc9ea088423eafba40f73f03
littleossa <littleossa@gmail.com>
2024-02-08 09:52:10 +0900

    Add Color+Extension systemBackground color

---
+++ SFSymbolsArtCollection/ArtisticWorldOfSFSymbols/Extensions/Color+.swift
@@ -0,0 +1,12 @@
+//
+//  Color+.swift
+//  SFSymbolsArtCollection
+//
+//  Created by Lil Ossa
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemBackground: Color = .init(uiColor: .systemBackground)
+}

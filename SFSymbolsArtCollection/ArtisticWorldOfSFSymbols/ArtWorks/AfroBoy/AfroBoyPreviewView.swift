29feddd05bcec4df93f34c8111276e6846ac0c88
littleossa <littleossa@gmail.com>
2024-02-08 21:10:08 +0900

    Refactor AfroBoyPreview

--- SFSymbolsArtCollection/ArtisticWorldOfSFSymbols/ArtWorks/AfroBoy/AfroBoyPreviewView.swift
+++ SFSymbolsArtCollection/ArtisticWorldOfSFSymbols/ArtWorks/AfroBoy/AfroBoyPreviewView.swift
@@ -17,35 +17,39 @@ struct AfroBoyPreviewView: View {
             
             // MARK: - 􁝰 Hair
             Image(systemName: "tree.fill")
-                .resizable()
-                .foregroundStyle(.black)
-                .frame(width: model.calculatingProportionalValue(withRatio: 0.9, forPreview: true),
-                       height: model.calculatingProportionalValue(withRatio: 0.9, forPreview: true))
+                .arrangeShape(
+                    width: model.calculatingProportionalValue(withRatio: 0.9, forPreview: true),
+                    height: model.calculatingProportionalValue(withRatio: 0.9, forPreview: true)
+                )
             
             // MARK: - 􀙧 Contour
             Image(systemName: "shield.fill")
-                .resizable()
-                .foregroundStyle(.paleOrange)
-                .frame(width: model.calculatingProportionalValue(withRatio: 0.5, forPreview: true),
-                       height: model.calculatingProportionalValue(withRatio: 0.6, forPreview: true))
-                .offset(y: model.calculatingProportionalValue(withRatio: 0.17, forPreview: true))
+                .arrangeShape(
+                    primaryColor: .paleOrange,
+                    width: model.calculatingProportionalValue(withRatio: 0.5, forPreview: true),
+                    height: model.calculatingProportionalValue(withRatio: 0.6, forPreview: true),
+                    verticalOffset: model.calculatingProportionalValue(withRatio: 0.17, forPreview: true)
+                )
             
             // MARK: - 􀎽 Face
             Image(systemName: "faceid")
-                .resizable()
-                .foregroundStyle(.black, .paleOrange)
-                .frame(width: model.calculatingProportionalValue(withRatio: 0.3, forPreview: true),
-                       height: model.calculatingProportionalValue(withRatio: 0.3, forPreview: true))
-                .offset(y: model.calculatingProportionalValue(withRatio: 0.16, forPreview: true))
+                .arrangeShape(
+                    primaryColor: .black,
+                    secondaryColor: .paleOrange,
+                    width: model.calculatingProportionalValue(withRatio: 0.3, forPreview: true),
+                    height: model.calculatingProportionalValue(withRatio: 0.3, forPreview: true),
+                    verticalOffset: model.calculatingProportionalValue(withRatio: 0.16, forPreview: true)
+                )
             
             // MARK: - 􀦉 Comb
             Image(systemName: "comb.fill")
-                .resizable()
-                .foregroundStyle(.red)
-                .frame(width: model.calculatingProportionalValue(withRatio: 0.2, forPreview: true),
-                       height: model.calculatingProportionalValue(withRatio: 0.2, forPreview: true))
-                .offset(x: model.calculatingProportionalValue(withRatio: -0.2, forPreview: true),
-                        y: model.calculatingProportionalValue(withRatio: -0.2, forPreview: true))
+                .arrangeShape(
+                    primaryColor: .red,
+                    width: model.calculatingProportionalValue(withRatio: 0.2, forPreview: true),
+                    height: model.calculatingProportionalValue(withRatio: 0.2, forPreview: true),
+                    horizontalOffset: model.calculatingProportionalValue(withRatio: -0.2, forPreview: true),
+                    verticalOffset: model.calculatingProportionalValue(withRatio: -0.2, forPreview: true)
+                )
         }
     }
 }
